d4b9ab8781240a6ef293475ea5f3002b6daf0f04
Devikaa D <devikaapreethi2000@gmail.com>
2023-05-02 20:36:36 +0530

    side nav updated

--- Dashboard  ReactJs/src/layouts/dashboard/config.js
+++ Dashboard  ReactJs/src/layouts/dashboard/config.js
@@ -28,7 +28,7 @@ export const items = [
     )
   },
   {
-    title: 'Learn & Practic',
+    title: 'Learn & Practice',
     path: '/companies',
     icon: (
       <SvgIcon fontSize="small">
@@ -54,31 +54,7 @@ export const items = [
       </SvgIcon>
     )
   },
-  {
-    title: 'Login',
-    path: '/auth/login',
-    icon: (
-      <SvgIcon fontSize="small">
-        <LockClosedIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Register',
-    path: '/auth/register',
-    icon: (
-      <SvgIcon fontSize="small">
-        <UserPlusIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Error',
-    path: '/404',
-    icon: (
-      <SvgIcon fontSize="small">
-        <XCircleIcon />
-      </SvgIcon>
-    )
-  }
+  
+  
+      
 ];
